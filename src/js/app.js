f421ca0d3f9cdc4a632979e7d589bd05090e4e6a
FUJII Kazunobu <fujii.kazunobu@gmail.com>
2016-01-04 15:01:23 +0900

    vue.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,27 @@
-import app from './index.js'
 window.$ = window.jQuery = require('jquery');
 var Bootstrap = require('bootstrap-sass');
 Bootstrap.$ = $;
+
+import Vue from 'vue'
+
+var demo = new Vue({
+  el: '#demo',
+  data: {
+    title: 'todos',
+    todos: [
+      {
+        done: true,
+        content: 'Learn JavaScript'
+      },
+      {
+        done: false,
+        content: 'Learn Vue.js'
+      }
+    ]
+  }
+});
+
+import app from './index.js'
 
 try {
   app.initialize();
