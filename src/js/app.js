ca0cc3bc00fd98dc6c510ec2ec0dc099c9ade9d2
Eliot Whalan <ewhal@pantsu.cat>
2016-05-13 22:49:15 +1000

    Finish up, add license

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,82 @@
-window.$ = window.jQuery = require('jquery');
-var Bootstrap = require('bootstrap-sass');
-Bootstrap.$ = $;
+document.addEventListener("deviceready", onDeviceReady, true);
+ 
+function id(element) {
+    return document.getElementById(element);
+}
 
-import Vue from 'vue'
+function onDeviceReady() {
+	cameraApp = new cameraApp();
+    cameraApp.run();
 
-var demo = new Vue({
-  el: '#demo',
-  data: {
-    title: 'todos',
-    todos: [
-      {
-        done: true,
-        content: 'Learn JavaScript'
-      },
-      {
-        done: false,
-        content: 'Learn Vue.js'
-      }
-    ]
-  }
-});
+    navigator.splashscreen.hide();
+	navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+}
 
-import app from './index.js'
 
-try {
-  app.initialize();
-} catch(e) {
-  console.log(e.stack);
+function onSuccess(position) {
+    var element = document.getElementById('geolocation');
+    element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+                        'Longitude: ' + position.coords.longitude     + '<br />' +
+                        '<hr />'      + element.innerHTML;
+}
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+
+// Options: throw an error if no update is received every 30 seconds.
+//
+var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+
+
+function cameraApp(){}
+
+cameraApp.prototype={
+    _pictureSource: null,
+    
+    _destinationType: null,
+    
+    run: function(){
+        var that=this;
+	    that._pictureSource = navigator.camera.PictureSourceType;
+	    that._destinationType = navigator.camera.DestinationType;
+		that._capturePhoto.apply(that,arguments);
+    },
+    
+    _capturePhoto: function() {
+        var that = this;
+        
+        // Take picture using device camera and retrieve image as base64-encoded string.
+        navigator.camera.getPicture(function(){
+            that._onPhotoDataSuccess.apply(that,arguments);
+        },function(){
+            that._onFail.apply(that,arguments);
+        },{
+            quality: 50,
+            destinationType: that._destinationType.DATA_URL
+        });
+    },
+    
+    _onPhotoDataSuccess: function(imageData) {
+        var smallImage = document.getElementById('smallImage');
+        smallImage.style.display = 'block';
+    
+        // Show the captured photo.
+        smallImage.src = "data:image/jpeg;base64," + imageData;
+    },
+    
+    _onPhotoURISuccess: function(imageURI) {
+        var smallImage = document.getElementById('smallImage');
+        smallImage.style.display = 'block';
+         
+        // Show the captured photo.
+        smallImage.src = imageURI;
+    },
+    
+    _onFail: function(message) {
+        alert(message);
+    }
 }
